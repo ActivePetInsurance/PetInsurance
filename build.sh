eaa173b96462dd683d33871a74d5cba92272707d
alexandernkingsbury <53443343+alexandernkingsbury@users.noreply.github.com>
2019-09-20 14:48:39 -0400

    comments

--- build.sh
+++ build.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 cd Pet-insurance-Angular/
+echo "here1"
 npm install
+echo "here2"
 npm run build
+echo "here3"
 cd ../
+echo "here4"
 rm -r node_modules
+echo "here5"
 gsutil cp -r dist gs://www.alexandernkingsburyprojects.com/Pet-insurance-Angular
+echo "here6"
+echo "here"
+echo "here"
+echo "here"
+echo "here"
+echo "here"
+echo "here"
