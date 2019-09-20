3502dd4ee705bf839775eee4dab85f48e5d4aab0
Blake Anthony Mongeon <bamongeon@gmail.com>
2019-09-20 13:46:02 -0400

    added line of code that unpacks the curled sdk

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@
 
 rm -rf "$HOME/google-cloud-sdk"
 sudo curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-263.0.0-linux-x86.tar.gz
+tar zxvf google-cloud-sdk-263.0.0-linux-x86.tar.gz google-cloud-sdk
 
 echo "installed successfully"
 
