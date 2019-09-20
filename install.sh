42d2e7b1c83df10d62cdd1aa51c95afee85eb951
Blake Anthony Mongeon <bamongeon@gmail.com>
2019-09-20 13:41:55 -0400

    added line to remove google coud sdk before installation

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@
 
 # source $HOME/google-cloud-sdk/path.bash.inc
 
+rm -rf "$HOME/google-cloud-sdk"
 sudo curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-263.0.0-linux-x86.tar.gz
 
 echo "installed successfully"
