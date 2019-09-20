c36f1e6f6aea087a971b17bdc181b07ba645c96f
Blake Anthony Mongeon <bamongeon@gmail.com>
2019-09-20 13:06:54 -0400

    added fail silently flag to the cur command

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 
 if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then
   rm -rf "$HOME/google-cloud-sdk"
-  curl https://sdk.cloud.google.com | bash > /dev/null
+  curl -f https://sdk.cloud.google.com | bash > /dev/null
 fi
 
 source $HOME/google-cloud-sdk/path.bash.inc
