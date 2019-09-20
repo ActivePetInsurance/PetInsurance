4fd119ab6383334407e2b103707a6c2065381f6a
Blake Anthony Mongeon <bamongeon@gmail.com>
2019-09-20 13:37:58 -0400

    added debugging line

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@
 
 sudo curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-263.0.0-linux-x86.tar.gz
 
+echo "installed successfully"
+
 # if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then
 #   rm -rf "$HOME/google-cloud-sdk"
 #   curl https://sdk.cloud.google.com | bash > /dev/null
