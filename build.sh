549c40d33d84807f089913cb52890e22b6ab8ac2
Blake Anthony Mongeon <bamongeon@gmail.com>
2019-09-23 10:09:04 -0400

    added ls for debugging

--- build.sh
+++ build.sh
@@ -6,10 +6,18 @@
 #rm -r node_modules
 #gsutil cp -r dist gs://www.alexandernkingsburyprojects.com/Pet-insurance-Angular
 
+ls
 cd Pet-insurance-Angular/
+ls
 npm install
+ls
 npm run build
+ls
 cd ../
+ls
 rm -r node_modules
 ls
+ls
+ls
 gsutil cp -r dist gs://www.alexandernkingsburyprojects.com/Pet-insurance-Angular
+ls
