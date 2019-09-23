d7753b0b8b07f4617835d135f1411a613d09f676
alexandernkingsbury <53443343+alexandernkingsbury@users.noreply.github.com>
2019-09-23 09:28:40 -0400

    Reverted back to almost the orginal version almost

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@
 #rm -r node_modules
 #gsutil cp -r dist gs://www.alexandernkingsburyprojects.com/Pet-insurance-Angular
 
-gcloud beta compute --project "projectthreeie" ssh --zone "us-central1-a" "travis"
-exit
-exit
+cd Pet-insurance-Angular/
+npm install
+npm run build
+cd ../
+rm -r node_modules
+gsutil cp -r dist gs://www.alexandernkingsburyprojects.com/Pet-insurance-Angular
