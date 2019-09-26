3646947c794ccd2271b0d7a808ad899790ab949c
Timothy Thumbi <timothythumbi@gmail.com>
2019-09-26 11:02:40 -0400

    email selection query

---
+++ RevaturePetInsurance/src/main/java/com/controller/OwnerController.java
@@ -0,0 +1,12 @@
+package com.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OwnerController {
+	
+	public void displayOwner(HttpServletRequest req, HttpServletResponse res) {
+		
+	}
+
+}
