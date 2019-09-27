387593bc27ea2c7613c7dfb9a2705ee40e3c6914
Blake Anthony Mongeon <bamongeon@gmail.com>
2019-09-27 15:22:27 -0400

    Changed authentication to container registry

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ yes | gcloud components install kubectl
 #gcloud auth configure-docker
 
 docker build -t gcr.io/projectthreeie/petinsurance:v1.0 .
-docker push gcr.io/projectthreeie/petinsurance:v1.0
+gcloud docker -- push gcr.io/projectthreeie/petinsurance:v1.0
 echo "k8s.yml"
 kubectl delete pods --all
 kubectl apply -f k8s.yml
