febc3496b387ad2f51ef74701f580eff75e598b3
Sabrina Suhartono <sabrina.m.suhartono@gmail.com>
2019-10-02 14:44:10 -0400

    forgot to change the endpoints

--- Pet-insurance-Angular/src/app/user-signup.service.ts
+++ Pet-insurance-Angular/src/app/user-signup.service.ts
@@ -18,8 +18,8 @@ export class UserSignupService {
       })
     };
 
-    return this.myHttpCli.post<string[]>('http://localhost:9005/RevaturePetInsurance/own/addOwner.app', newUser, httpPut);
-    // return this.myHttpCli.post<string[]>('http://35.232.12.74/RevaturePetInsurance-0.0.1-SNAPSHOT/own/addOwner.app', newUser, httpPut);
+    // return this.myHttpCli.post<string[]>('http://localhost:9005/RevaturePetInsurance/own/addOwner.app', newUser, httpPut);
+    return this.myHttpCli.post<string[]>('http://35.232.12.74/RevaturePetInsurance-0.0.1-SNAPSHOT/own/addOwner.app', newUser, httpPut);
   }
 }
 
