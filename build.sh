9cbd191e28c524dcb5df507c13516e5657ec424c
alexandernkingsbury <53443343+alexandernkingsbury@users.noreply.github.com>
2019-10-03 09:17:55 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,11 @@ cd RevaturePetInsurance/
 mvn package
 cd ../
 
+#go into the folder
 cd Pet-insurance-Angular/
+#sometimes npm doesn't work unless it is freshly installed
 npm install
+#run the anglar
 npm run build
+#put the contents of the anglar into the bucket
 gsutil cp -r dist/class-example/* gs://www.alexandernkingsburyprojects.com
